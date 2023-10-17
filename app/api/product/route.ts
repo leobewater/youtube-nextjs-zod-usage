c8080e5d12224dd41058c2046f38bb23a5835a00
leobewater <mmbee888@gmail.com>
2023-10-17 09:44:28 -0400

    add zod

--- app/api/product/route.ts
+++ app/api/product/route.ts
@@ -2,9 +2,10 @@ import { NextResponse } from 'next/server';
 
 export async function GET(request: Request) {
   const product = {
-    name: 'Cool jeans',
+    //name: 'Cool jeans',
+    id: '1',
     price: 100,
   };
-    
+
   return NextResponse.json(product);
 }
--- app/components/product.tsx
+++ app/components/product.tsx
@@ -2,9 +2,21 @@
 
 import { useEffect } from 'react';
 
+// type script not enough to validate the api response, what happen if the api removed one of the key such as name???
+// TS doesn't check the type at runtime
+type Product = {
+  name: string;
+  price: number;
+};
+
 const Product = () => {
   useEffect(() => {
-    fetch('/api/product');
+    // typescript return api as "any" type
+    fetch('/api/product')
+      .then((res) => res.json())
+      .then((product: Product) => {
+        console.log(product.name?.toUpperCase());
+      });
   }, []);
   return <div>product</div>;
 };
