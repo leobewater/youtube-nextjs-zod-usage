845a805aaa92e77b8e7403f77881341fffa33e01
leobewater <mmbee888@gmail.com>
2023-10-17 09:53:23 -0400

    z.infer

--- app/components/product.tsx
+++ app/components/product.tsx
@@ -5,10 +5,10 @@ import { z } from 'zod';
 
 // type script not enough to validate the api response, what happen if the api removed one of the key such as name???
 // TS doesn't check the type at runtime
-type Product = {
-  name: string;
-  price: number;
-};
+// type Product = {
+//   name: string;
+//   price: number;
+// };
 
 // This run in runtime
 const productSchema = z.object({
@@ -16,12 +16,20 @@ const productSchema = z.object({
   price: z.number(),
 });
 
+// use z.infer to get the type for Product instead of re-creating it separately and make the z.object source of truth
+type Product = z.infer<typeof productSchema>;
+
+// should we re-create the Product type (duplication)
+const getPriceFromProduct = (product: Product) => {
+  return product.price;
+};
+
 const Product = () => {
   useEffect(() => {
     // typescript return api as "any" type
     fetch('/api/product')
       .then((res) => res.json())
-      .then((product: Product) => {
+      .then((product: unknown) => {
         // use Zod to validate the product
 
         // use safeParse without throwing errors and check with the .success property
@@ -31,9 +39,9 @@ const Product = () => {
           return;
         }
 
-          // use the product and type hinting is working
-          console.log(validatedProduct.data.name);
-          console.log(validatedProduct.data.price);
+        // use the product and type hinting is working
+        console.log(validatedProduct.data.name);
+        console.log(validatedProduct.data.price);
       });
   }, []);
   return <div>product</div>;
