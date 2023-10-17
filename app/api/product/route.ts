aace543cd332e7c5e0129837a3dbcb76082bd370
leobewater <mmbee888@gmail.com>
2023-10-17 10:23:22 -0400

    validate api post request

--- app/api/product/route.ts
+++ app/api/product/route.ts
@@ -1,3 +1,4 @@
+import { checkoutFormSchema } from '@/app/lib/validations';
 import { NextResponse } from 'next/server';
 
 export async function GET(request: Request) {
@@ -8,4 +9,16 @@ export async function GET(request: Request) {
   };
 
   return NextResponse.json(product);
+}
+
+// post method, receiving data
+export async function POST(request: Request, response: NextResponse) {
+  const body: unknown = await request.json();
+
+  const parsedForm = checkoutFormSchema.safeParse(body);
+  if (!parsedForm.success) {
+    return NextResponse.json(parsedForm.error, { status: 422 });
+  }
+
+  return NextResponse.json(parsedForm);
 }
