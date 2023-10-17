89d49d598bea4eb216b8a7f12208341aeb8b4c26
leobewater <mmbee888@gmail.com>
2023-10-17 09:38:09 -0400

    set up product api

--- app/components/product.tsx
+++ app/components/product.tsx
@@ -1,7 +1,12 @@
 'use client';
 
-const product = () => {
+import { useEffect } from 'react';
+
+const Product = () => {
+  useEffect(() => {
+    fetch('/api/product');
+  }, []);
   return <div>product</div>;
 };
 
-export default product;
+export default Product;
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
+import Product from './components/product';
+
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      Hello World!
+      <Product />
     </main>
   );
 }
