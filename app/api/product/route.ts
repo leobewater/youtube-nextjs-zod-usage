498a72dd80b20289d3058c4d49ae5bbeee76d921
leobewater <mmbee888@gmail.com>
2023-10-17 09:50:02 -0400

    validate backend api

--- app/api/product/route.ts
+++ app/api/product/route.ts
@@ -2,8 +2,8 @@ import { NextResponse } from 'next/server';
 
 export async function GET(request: Request) {
   const product = {
-    //name: 'Cool jeans',
-    id: '1',
+    name: 'Cool jeans',
+    //id: '1',
     price: 100,
   };
 
--- app/components/product.tsx
+++ app/components/product.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import { useEffect } from 'react';
+import { z } from 'zod';
 
 // type script not enough to validate the api response, what happen if the api removed one of the key such as name???
 // TS doesn't check the type at runtime
@@ -9,13 +10,30 @@ type Product = {
   price: number;
 };
 
+// This run in runtime
+const productSchema = z.object({
+  name: z.string(),
+  price: z.number(),
+});
+
 const Product = () => {
   useEffect(() => {
     // typescript return api as "any" type
     fetch('/api/product')
       .then((res) => res.json())
       .then((product: Product) => {
-        console.log(product.name?.toUpperCase());
+        // use Zod to validate the product
+
+        // use safeParse without throwing errors and check with the .success property
+        const validatedProduct = productSchema.safeParse(product);
+        if (!validatedProduct.success) {
+          console.error(validatedProduct.error);
+          return;
+        }
+
+          // use the product and type hinting is working
+          console.log(validatedProduct.data.name);
+          console.log(validatedProduct.data.price);
       });
   }, []);
   return <div>product</div>;
